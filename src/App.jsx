3bf32a40adfa758d686784cff715a06550cef54f
Mukaddes Kurter <mukaddeskurter@turev.com.tr>
2026-01-05 15:01:49 +0300

    Ana sayfayı experience'a yönlendir

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import ExperiencePage from "./pages/ExperiencePage";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<div>Ana Sayfa</div>} />
+      <Route path="/" element={<Navigate to="/experience" replace />} />
       <Route path="/experience" element={<ExperiencePage />} />
     </Routes>
   );
